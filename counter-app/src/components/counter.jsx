5bf030b891b9d31e0f93d7ad7d21dcc42ebd0d2c
Danny Navejas <dannynavejas@gmail.com>
2019-07-01 18:03:29 -0500

    refactored code to make a new function to check state of count and change class based on that state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,14 +4,21 @@ class Counter extends Component {
   state = {
     count: 0
   };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btb-sm">Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
