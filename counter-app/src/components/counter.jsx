c2ce76e0a4ea7abf6491c21071ead0adb5151568
Danny Navejas <dannynavejas@gmail.com>
2019-07-01 18:49:45 -0500

    added code to increment count and display current count

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,12 +5,20 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btb-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btb-sm"
+        >
+          Increment
+        </button>
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
